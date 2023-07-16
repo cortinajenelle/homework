c123596139055ffb7761fc27ca84c86519b1a48b
Cortina Caldwell <cortinajenellecaldwell@gmail.com>
2023-07-16 16:59:15 -0400

    Week 5

--- Week 5/Week5-Homework/Array-Methods/Post.js
+++ Week 5/Week5-Homework/Array-Methods/Post.js
@@ -1,1 +1,17 @@
 // In the Post.js file, use the placeholder Post data at https://jsonplaceholder.typicode.com/posts?_limit=50 to:
+
+// create a list of user 5's posts
+
+const newArray = data.filter(task => {
+    return task.userId == 5;
+});
+
+// then, create a list of title and body pairs
+
+const concept = [];
+data.forEach(posts => {
+    let title = posts.title;
+    let body = posts.body;
+
+concept.push([title,body]);
+});
--- Week 5/Week5-Homework/Array-Methods/User.js
+++ Week 5/Week5-Homework/Array-Methods/User.js
@@ -1,1 +1,16 @@
 // In the User.js file, use the placeholder User data at https://jsonplaceholder.typicode.com/users to:
+
+// create and print a list of phone numbers
+
+data.forEach(user => console.log(user.phone));
+
+// create and print a list of website and email pairs (pair them in a string, array, or object)
+
+const about = [];
+data.forEach(user => {
+    let email = user.email;
+    let website = user.website;
+
+about.push([email,website]);
+});
+console.log(about);
